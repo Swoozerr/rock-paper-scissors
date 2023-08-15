3ce19decb9cbd36db5e8505b4ec5e3949ac00540
L <123719357+Swoozerr@users.noreply.github.com>
2023-08-15 18:42:52 -0500

    function playRound() completed

--- rps.js
+++ rps.js
@@ -4,9 +4,30 @@
  * @javadoc style comments if i wasn't lazy
 */ 
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ["rock", "paper", "scissors"];
     const num = Math.floor(Math.random() * 3);
     return choices[num];
 }
 
-console.log(getComputerChoice());
+/**
+ *  Plays a round given playerSelection and computerSelection
+ * 
+ * @javadoc style comments if i wasn't lazy
+*/ 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) return "Draw";
+
+    if (playerSelection === "rock") {
+        if(computerSelection === "paper") return "Lost, paper > rock";
+        else return "Win, rock > scissors";
+    }
+    else if (playerSelection === "paper"){
+        if(computerSelection === "scissors") return "Lost, scisors > paper";
+        else return "Win, paper > rock";
+    }
+    else if (playerSelection === "scissors"){
+        if(computerSelection === "rock") return "Lost, rock > scissors";
+        else return "Win, scissors > paper";
+    }
+}
